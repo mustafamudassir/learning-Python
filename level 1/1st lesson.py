fff781076910447a033daf4dfc1927682476cf53
mustafamudassir <85429074+mustafamudassir@users.noreply.github.com>
2021-06-07 16:27:33 +0530

    Add files via upload

---
+++ level 1/1st lesson.py
@@ -0,0 +1,71 @@
+#
+# n = 5
+# while n > 0 :
+#     print(n)
+#     n = n - 1
+#     print(' mustafa ')
+
+# xx = 2
+# xx = xx + 2
+# print(xx)
+# yy = 440 * 12
+# print(yy)
+# zz = yy / 1000
+# print(zz)
+
+# jj = 26
+# kk = jj % 5
+# print(kk)
+#
+# hours = 35.0
+# rate = 12.5
+# pay = hours * rate
+# print(pay)
+
+# ORDER OF EVALUATION
+
+# x = 1 + 2 ** 2 / 4 + 4
+# print(x)
+# ee = 'str'
+# print(type(eee))
+# # <class'str'>
+# type('hello')
+# # class'str'>
+# print(type(1))
+# # <class'int'>
+
+
+# name = input('who are you')
+# print('welcome',name )
+#
+
+# CONVERTING USER INPUT
+
+
+# inp = input('europe floor?')
+# floor = int(inp) + 1
+# print('us floor',floor)
+
+# IF STATEMENT
+#
+# x = 5
+# if x < 10:
+#     print('smaller')
+#     if x > 20:
+#         print('bigger')
+# print('none')
+#
+# COMPARISION OPERATORS
+
+# x = 6
+# if x >= 6:
+#     print('equal')
+#
+# y = 4
+# if y == 4:
+#     print('equals')
+#
+# z = 10
+# if z != 10:
+#     print('not equal')
+
