321caba846c1a45da3c2553e27062aafcc3c6932
mustafamudassir <85429074+mustafamudassir@users.noreply.github.com>
2021-06-29 11:40:51 +0530

    Update learning python.py

--- level 1/learning python.py
+++ level 1/learning python.py
@@ -7,3 +7,31 @@
 
 # mustafa = ['2', '2', '2']
 # mustafa[2] = 1
+
+# t = [1, 2, 3, 4, 5]
+# if 6 in t:
+#     print('true')
+# else:
+#     print('false')
+#
+
+#
+# total = 0
+# count = 0
+# while True:
+#     inp = input('enter a number:')
+#     if inp == 'done' : break
+#     value = float(inp)
+#     total = total + value
+#     count = count + 1
+#
+# average = total / count
+# print('Average:', average)5
+
+abc = 'm u s'
+stuff = abc.split()
+print(stuff)
+print(stuff[2])
+print(len(stuff))
+
+for m in stuff:
